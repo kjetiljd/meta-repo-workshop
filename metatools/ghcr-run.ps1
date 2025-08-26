da093198da44440ba743c1046e0202cbe48bb50d
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2025-08-26 10:20:07 +0200

    Add Git user configuration to Docker run scripts

--- metatools/ghcr-run.ps1
+++ metatools/ghcr-run.ps1
@@ -1,2 +1,9 @@
-docker run --rm -it -v "$($PWD.Path):/work" -w /work ghcr.io/kjetiljd/meta-repo-workshop-tools:latest bash
+$gitUserName = try { git config user.name } catch { "" }
+$gitUserEmail = try { git config user.email } catch { "" }
+
+docker run --rm -it `
+  -v "$($PWD.Path):/work" -w /work `
+  -e "GIT_USER_NAME=$gitUserName" `
+  -e "GIT_USER_EMAIL=$gitUserEmail" `
+  ghcr.io/kjetiljd/meta-repo-workshop-tools:latest bash
 
--- metatools/ghcr-run.sh
+++ metatools/ghcr-run.sh
@@ -1,2 +1,6 @@
-docker run --rm -it -v "$PWD:/work" -w /work ghcr.io/kjetiljd/meta-repo-workshop-tools:latest bash
+docker run --rm -it \
+  -v "$PWD:/work" -w /work \
+  -e "GIT_USER_NAME=$(git config user.name || echo '')" \
+  -e "GIT_USER_EMAIL=$(git config user.email || echo '')" \
+  ghcr.io/kjetiljd/meta-repo-workshop-tools:latest bash
 
